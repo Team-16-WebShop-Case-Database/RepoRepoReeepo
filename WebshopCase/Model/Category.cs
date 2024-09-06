d949bfacfb6ac428ea3b8ff05cc7e109fd38f52c
Tdionhogh <159053190+Tdionhogh@users.noreply.github.com>
2024-09-06 15:52:46 +0200

    Heyyyyyy

---
+++ WebshopCase/Model/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopCase.Model
+{
+    public class Category
+    {
+        public int CategoryId {  get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category() { 
+        
+            Products = new List<Product>();
+        }
+
+        public Category(string CategoryName, string CategoryDescription)
+        {
+            CategoryName = CategoryName;
+            CategoryDescription = CategoryDescription;
+
+
+        }
+
+
+    }
+}
